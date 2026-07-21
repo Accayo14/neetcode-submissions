f24e8be33716bc483bd3897f6f3b711d29edab2a
Aryan Shah <aryan.cshah1408@gmail.com>
2026-07-21 12:58:41 +0530

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int, int>> cars;
+        int n = position.size();
+
+        for(int i = 0; i < n; i++){
+            position[i] = target - position[i];
+            cars.push_back({position[i], speed[i]});
+        }
+
+        sort(cars.begin(), cars.end());
+
+        vector<float> time(n);
+        for(int i = 0; i < n; i++){
+            time[i] = (float)cars[i].first / (float)cars[i].second;
+        }
+
+        int fleet = 1;
+        float tmin = time[0];
+        for(int i = 1; i < n; i++){
+            if(time[i] <= tmin) continue;
+            else{
+                fleet++;
+                tmin = time[i];
+            }
+        }
+
+        return fleet;
+    }
+};
